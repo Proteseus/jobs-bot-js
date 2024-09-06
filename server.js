b212e2d56a9b8eb1aad89479b1e5039ffa1cbf7f
Proteseus <lewiwui@gmail.com>
2024-09-06 14:20:07 +0300

    added webhook handler tester 1.2

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
 import express from "express";
 import dotenv from 'dotenv';
-import { Telegraf } from "telegraf";
+import { Scenes, session, Telegraf } from "telegraf";
+
+// Handler factories
+const { enter, leave } = Scenes.Stage;
 
 dotenv.config();
 const port = 3000;
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 const app = express();
 
-// Set the bot API endpoint
-const webhookDomain = 'https://jobs-bot-js.onrender.com';
-app.use(await bot.createWebhook({ domain: webhookDomain }));
+// Greeter scene
+const greeterScene = new Scenes.BaseScene<Scenes.SceneContext>("greeter");
+greeterScene.enter(ctx => ctx.reply("Hi"));
+greeterScene.leave(ctx => ctx.reply("Bye"));
+greeterScene.hears("hi", enter<Scenes.SceneContext>("greeter"));
+greeterScene.on("message", ctx => ctx.replyWithMarkdown("Send `hi`"));
 
-bot.on("text", ctx => ctx.reply("Hello"));
+// Echo scene
+const echoScene = new Scenes.BaseScene<Scenes.SceneContext>("echo");
+echoScene.enter(ctx => ctx.reply("echo scene"));
+echoScene.leave(ctx => ctx.reply("exiting echo scene"));
+echoScene.command("back", leave<Scenes.SceneContext>());
+echoScene.on("text", ctx => ctx.reply(ctx.message.text));
+echoScene.on("message", ctx => ctx.reply("Only text messages please"));
+
+const bot = new Telegraf<Scenes.SceneContext>(process.env.TELEGRAM_BOT_TOKEN);
+
+const stage = new Scenes.Stage<Scenes.SceneContext>([greeterScene, echoScene], {
+    ttl: 10,
+});
+
+bot.use(session());
+bot.use(stage.middleware());
+bot.command("greeter", ctx => ctx.scene.enter("greeter"));
+bot.command("echo", ctx => ctx.scene.enter("echo"));
+bot.on("message", ctx => ctx.reply("Try /echo or /greeter"));
+
+// Set the bot API endpoint
+const webhookDomain = "https://jobs-bot-js.onrender.com";
+app.use(await bot.createWebhook({ domain: webhookDomain }));
 
 app.listen(port, () => console.log("Listening on port", port));
