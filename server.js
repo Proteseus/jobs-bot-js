2bc4ed369ee4345ce5fd00c4274be11f51fae0c6
Proteseus <lewiwui@gmail.com>
2024-09-06 14:52:38 +0300

    added webhook handler tester 1.2.1

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ const port = 3000;
 const app = express();
 
 // Greeter scene
-const greeterScene = new Scenes.BaseScene<Scenes.SceneContext>("greeter");
+const greeterScene = new Scenes.BaseScene("greeter");
 greeterScene.enter(ctx => ctx.reply("Hi"));
 greeterScene.leave(ctx => ctx.reply("Bye"));
-greeterScene.hears("hi", enter<Scenes.SceneContext>("greeter"));
+greeterScene.hears("hi", enter("greeter"));
 greeterScene.on("message", ctx => ctx.replyWithMarkdown("Send `hi`"));
 
 // Echo scene
-const echoScene = new Scenes.BaseScene<Scenes.SceneContext>("echo");
+const echoScene = new Scenes.BaseScene("echo");
 echoScene.enter(ctx => ctx.reply("echo scene"));
 echoScene.leave(ctx => ctx.reply("exiting echo scene"));
-echoScene.command("back", leave<Scenes.SceneContext>());
+echoScene.command("back", leave());
 echoScene.on("text", ctx => ctx.reply(ctx.message.text));
 echoScene.on("message", ctx => ctx.reply("Only text messages please"));
 
-const bot = new Telegraf<Scenes.SceneContext>(process.env.TELEGRAM_BOT_TOKEN);
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-const stage = new Scenes.Stage<Scenes.SceneContext>([greeterScene, echoScene], {
+const stage = new Scenes.Stage([greeterScene, echoScene], {
     ttl: 10,
 });
 
